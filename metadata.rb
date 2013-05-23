7c4b4b795f0439229e5a538861fe9aa4fe72cb2c
Dan Buch <d.buch@modcloth.com>
2013-05-22 21:55:58 -0400

    Meant to explicitly set the name.  Stating supported platform is nice.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,9 @@
+name             "logadm"
 maintainer       "ModCloth, Inc."
 maintainer_email "platformsphere+logadm-cookbook@modcloth.com"
 license          "MIT"
 description      "Installs/Configures logadm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.1"
+
+supports "smartos"
