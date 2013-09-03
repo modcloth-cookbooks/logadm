241325c37fa388e99a11d494d5a010d94e150c54
Dan Buch <d.buch@modcloth.com>
2013-09-03 11:46:53 -0400

    Some minor touchups, but no functionality change
    including removing tabs and replacing double with single quotes.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name             "logadm"
-maintainer       "ModCloth, Inc."
-maintainer_email "platformsphere+logadm-cookbook@modcloth.com"
-license          "MIT"
-description      "Installs/Configures logadm"
+name             'logadm'
+maintainer       'ModCloth, Inc.'
+maintainer_email 'github+logadm-cookbook@modcloth.com'
+license          'MIT'
+description      'Installs/Configures logadm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          '0.1.2'
 
-supports "smartos"
+supports 'smartos'
